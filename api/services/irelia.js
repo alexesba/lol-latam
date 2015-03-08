5e9e2c788f60dc0796c07f2671f740476a8d13b9
alexesba <alexesba@gmail.com>
2015-03-08 13:17:02 -0600

    added config files for irelia lol api

---
+++ api/services/irelia.js
@@ -0,0 +1,13 @@
+var Irelia = require('irelia');
+var configIrelia = require('nconf');
+
+configIrelia.argv().env().file({ file: 'config/lolcredentials.json'});
+
+
+exports.lolAPI = new Irelia({
+  secure: true,
+  host: configIrelia.get('host'),
+  path: configIrelia.get('path'),
+  key: configIrelia.get('key'),
+  debug: configIrelia.get('debug')
+});
