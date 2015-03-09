317c69c28865a31d884d841eaf428ee205dc07a0
alexesba <alexesba@gmail.com>
2015-03-08 19:40:44 -0600

    fix test description

--- test/unit/models/user.test.js
+++ test/unit/models/user.test.js
@@ -2,7 +2,7 @@ require("sails-test-helper");
 
 
 describe(TEST_NAME, function(){
-  it('can be stored if the user email is invalid', function(done){
+  it('can\'t be stored if the user email is invalid', function(done){
       User.create({ email: 'wrongemail', name: 'demo'}, function(err, user){
         expect(user).to.not.exist;
         expect(err.Errors.email).to.exist;
