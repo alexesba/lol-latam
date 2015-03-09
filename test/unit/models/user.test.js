60d4f8b7ae6a7318d98e933185926249f1be7937
alexesba <alexesba@gmail.com>
2015-03-08 19:44:06 -0600

    add describe for create a user

--- test/unit/models/user.test.js
+++ test/unit/models/user.test.js
@@ -2,13 +2,15 @@ require("sails-test-helper");
 
 
 describe(TEST_NAME, function(){
-  it('can\'t be stored if the user email is invalid', function(done){
-      User.create({ email: 'wrongemail', name: 'demo'}, function(err, user){
-        expect(user).to.not.exist;
-        expect(err.Errors.email).to.exist;
-        expect(err.Errors.email[0].message).
-                 to.equal(User.validationMessages.email.email)
-        done();
-      });
+  describe('.create()', function(){
+    it('can\'t be stored if the user email is invalid', function(done){
+        User.create({ email: 'wrongemail', name: 'demo'}, function(err, user){
+          expect(user).to.not.exist;
+          expect(err.Errors.email).to.exist;
+          expect(err.Errors.email[0].message).
+                   to.equal(User.validationMessages.email.email)
+          done();
+        });
+    });
   });
 });
